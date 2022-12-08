10f70853214ad58ffc59fa82b6a6d941a47643db
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-08 14:53:00 -0300

    data added dinamically

--- 01-starting-setup/src/components/ReadedPages.js
+++ 01-starting-setup/src/components/ReadedPages.js
@@ -1,12 +1,16 @@
 import './ReadedPages.css'
 
 function ReadedPages() {
+    const readedPagesDate = new Date(2022, 2, 2).toDateString();
+    const course = 'Historia';
+    const readedPagesQty = 24;
+
   return (
     <div className='readed-pages-item'>
-      <div>25 de Marzo de 2021</div>
+      <div>{readedPagesDate}</div>
       <div className='readed-pages-item__description'>
-        <h2>Historia Argentina</h2>
-        <div className='readed-pages-item__price'>27</div>
+        <h2>{course}</h2>
+        <div className='readed-pages-item__price'>{readedPagesQty}</div>
       </div>
     </div>
   );
